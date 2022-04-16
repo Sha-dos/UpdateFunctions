7e00fefcfd2a69c2b451559ba6483ecf73fffca3
Sha-dos <77746688+Sha-dos@users.noreply.github.com>
2022-04-16 16:58:43 -0500

    added files to work on other computer

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,14 @@
 use std::thread;
 use std::time::Duration;
 
+/// Update is a function that calls or "updates" another function
 pub fn update(duration: Duration, function: fn()) {
     function();
     thread::sleep(duration);
     update(duration, function);
 }
 
+/// Update_True is a function that calls or "updates" another function if a bool is true
 pub fn update_true(duration: Duration, statement: bool, function: fn()) {
     if statement == true { 
         function(); 
